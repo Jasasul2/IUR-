6e140987ade00eb48c09799271c274e42c6574dc
Zonder <macesond@fel.cvut.cz>
2023-12-16 13:59:57 +0100

    Added get XP and LEVEL up binding methods

--- IUR_macesond_NET6/ViewModels/MainViewModel.cs
+++ IUR_macesond_NET6/ViewModels/MainViewModel.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows;
 
 namespace IUR_macesond_NET6.ViewModels
 {
@@ -20,7 +21,9 @@ namespace IUR_macesond_NET6.ViewModels
 
         public UserSettingsViewModel UserSettings { get; set; } = new UserSettingsViewModel();
 
-        #region LevelProperties
+        #region XPAndLevelProperties
+        private readonly int XP_LEVEL_INCREASER = 5;
+
         private int _currentLevel;
         private int _currentXP;
         private int _nextLevelXP;
@@ -42,31 +45,29 @@ namespace IUR_macesond_NET6.ViewModels
             get => _nextLevelXP;
             set => SetProperty(ref _nextLevelXP, value);
         }
+
+        private void LevelUp()
+        {
+            CurrentLevel++;
+            NextLevelXP += XP_LEVEL_INCREASER;
+        }
+
+        public void AddEXP(int newXP)
+        {
+            CurrentXP += newXP;
+            if (CurrentXP >= NextLevelXP)
+            {
+                CurrentXP = CurrentXP - NextLevelXP;
+                LevelUp();
+            }
+        }
         #endregion
-
-        //    //public ObservableCollection<WeatherCardViewModel> WeatherCards { get; set; } = new ObservableCollection<WeatherCardViewModel>();
-        //    private RelayCommand _AddTaskCommand;
-        //    public RelayCommand AddTaskCommand
-        //    {
-        //        get { return _AddTaskCommand ?? (_AddTaskCommand = new RelayCommand(AddTask, AddTaskCanExecute)); }
-        //    }
-
-        //    private void AddTask(object obj)
-        //    {
-        //        //WeatherCards.Add(new WeatherCardViewModel(this, CityToBeAdded));
-        //        //CityToBeAdded = "";
-        //    }
-
-        //    private bool AddTaskCanExecute(object obj)
-        //    {
-        //        return true;
-        //    }
 
         public MainViewModel()
         {
-            CurrentLevel = 5;
-            CurrentXP = 32;
-            NextLevelXP = 100;
+            CurrentLevel = 1;
+            CurrentXP = 5;
+            NextLevelXP = 10;
         }
     }
 }
--- IUR_macesond_NET6/ViewModels/TaskViewModel.cs
+++ IUR_macesond_NET6/ViewModels/TaskViewModel.cs
@@ -9,81 +9,25 @@ namespace IUR_macesond_NET6.ViewModels
 {
     internal class TaskViewModel : ViewModelBase
     {
+        private MainViewModel _mainViewModelReference;
+
+        public enum Difficulty
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+
+        private Dictionary<Difficulty, int> DifficultyToExp = new Dictionary<Difficulty, int>()
+        {
+            { Difficulty.Easy, 3 },
+            { Difficulty.Medium, 5 },
+            { Difficulty.Hard, 10 }
+        };
+
         public TaskViewModel(MainViewModel mainViewModelReference, string location)
         {
             _mainViewModelReference = mainViewModelReference;
-            Location = location;
-        }
-
-        private RelayCommand _removeCityCommand;
-        private MainViewModel _mainViewModelReference;
-        private string _location;
-        private double _temperature;
-        private double _humidity;
-        private string _conditions;
-        private string _iconPath;
-
-        public double Temperature
-        {
-            get => _temperature;
-            set => SetProperty(ref _temperature, value);
-        }
-
-        public double Humidity
-        {
-            get => _humidity;
-            set => SetProperty(ref _humidity, value);
-        }
-
-        public string Conditions
-        {
-            get => _conditions;
-            set => SetProperty(ref _conditions, value);
-        }
-
-        public string IconPath
-        {
-            get => _iconPath;
-            set => SetProperty(ref _iconPath, value);
-        }
-
-        public string Location
-        {
-            get => _location;
-            set
-            {
-                SetProperty(ref _location, value);
-                UpdateWeather();
-            }
-        }
-
-        public RelayCommand RemoveCityCommand
-        {
-            get { return _removeCityCommand ?? (_removeCityCommand = new RelayCommand(RemoveCity, RemoveCityCanExecute)); }
-        }
-
-        private void RemoveCity(object obj)
-        {
-            //_mainViewModelReference.WeatherCards.Remove(this);
-        }
-
-        private bool RemoveCityCanExecute(object obj)
-        {
-            return true;
-        }
-
-        private void UpdateWeather()
-        {
-            //var singleResult = WeatherNet.Current.GetByCityName(Location, "Czechia", "en", "metric");
-
-            //if (singleResult.Success)
-            //{
-            //    Temperature = singleResult.Item.Temp;
-            //    Humidity = singleResult.Item.Humidity;
-            //    Conditions = singleResult.Item.Description;
-            //    IconPath = singleResult.Item.Icon;
-            //    //_location = singleResult.Item.City; // FIX to avoid loop
-            //}
         }
     }
 }

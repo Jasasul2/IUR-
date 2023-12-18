05d7ac4f502bbf690670820b8c513f53fd1a8dd0
Zonder <macesond@fel.cvut.cz>
2023-12-18 14:37:10 +0100

    Added a time input tooltip translation

--- IUR_macesond_NET6/ValidationRules/HourValidationRule.cs
+++ IUR_macesond_NET6/ValidationRules/HourValidationRule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using IUR_macesond_NET6.Converters;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace IUR_macesond_NET6.ValidationRules
                 }
             }
 
-            return new ValidationResult(false, "Please enter a valid number from 0 to 23.");
+            return new ValidationResult(false, Translator.TranslateToCzech("Please enter a valid number from 0 to 23."));
         }
     }
 }

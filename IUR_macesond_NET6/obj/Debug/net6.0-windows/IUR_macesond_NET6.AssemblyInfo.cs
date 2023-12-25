f366527eb9783ba3ed5fd1b942a64902fae2fb97
Zonder <macesond@fel.cvut.cz>
2023-12-25 17:52:12 +0100

    Fixed a bug where time input in task library wasn't correctly disabled

--- IUR_macesond_NET6/obj/Debug/net6.0-windows/IUR_macesond_NET6.AssemblyInfo.cs
+++ IUR_macesond_NET6/obj/Debug/net6.0-windows/IUR_macesond_NET6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IUR_macesond_NET6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2015b617bb6b3bd289ea0cf41ab082d1daeb5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IUR_macesond_NET6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IUR_macesond_NET6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

7a5aa4ed6c4e39b3e0cbc0f915f7bcb8eb606676
Zonder <macesond@fel.cvut.cz>
2023-12-26 20:28:31 +0100

    downloaded a notificitaion package

--- IUR_macesond_NET6/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ IUR_macesond_NET6/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -731,7 +731,7 @@ namespace IUR_macesond_NET6 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/IUR_macesond_NET6;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/IUR_macesond_NET6;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
